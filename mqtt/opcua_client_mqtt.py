98cd6dc03f73035f9bc0d057b0c12cef35e4aa57
IlyaMarvin <ilya.sereda2002@mail.ru>
2022-05-20 00:46:34 +0300

    Add database structure

--- mqtt/opcua_client_mqtt.py
+++ mqtt/opcua_client_mqtt.py
@@ -2,17 +2,31 @@ from opcua import Client
 import paho.mqtt.client as mqtt
 import time
 import json
+import psycopg2
 
+# Параметры базы данных
+DB_HOST = ''
+DB_NAME = ''
+DB_USER = ''
+DB_PASS = ''
+
+# Соединение с OPC UA сервером
 url = "opc.tcp://localhost:4840"
 client = Client(url)
 client.connect()
 print("OPC UA Client connected")
 
+# Соединение с Thingsboard через MQTT
 iot_hub = "demo.thingsboard.io"
 port = 1883
 username = "e7UD6nDAzcFpTSu7lW8C"
 password = ""
 topic = "v1/devices/me/telemetry"
+
+# Соединение с базой данных по нашим данным
+conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
+# Создание курсора
+cur = conn.cursor()
 
 iot_hub_client = mqtt.Client()
 iot_hub_client.username_pw_set(username, password)
@@ -20,6 +34,7 @@ iot_hub_client.connect(iot_hub, port)
 print("Connected to IOT hub")
 
 data = dict()
+
 while True:
     try:
         temp = client.get_node("ns=2;i=2")
@@ -35,6 +50,7 @@ while True:
         data["height"] = int(height)
         data_out = json.dumps(data)
         iot_hub_client.publish(topic, data_out, 0)
+
 
         time.sleep(2)
 
